192752e7c6bdd030f657696cc7a0748d403b35d0
Alexander Suesskind <s87424@bht-berlin.de>
2024-11-01 13:48:36 +0100

    Initial commit

---
+++ src/test/kotlin/com/suesskind/urlshortener/UrlshortenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.suesskind.urlshortener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
